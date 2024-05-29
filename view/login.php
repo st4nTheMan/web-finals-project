d095d7770c813c65687c86f6b0d718d97d2da24c
JohnlyndelCastillo <johnlyndelcastillo@gmail.com>
2024-05-29 13:21:34 +0800

    Changes to css linking

--- view/login.php
+++ view/login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-    <link rel="stylesheet" href="../css/login_style.css">
+    <link rel="stylesheet" href="css/login_style.css">
 </head>
 
 <body>
