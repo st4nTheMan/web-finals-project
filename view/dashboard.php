24501eb3a63ddb4f2125e9ad8661bc4e3c95775a
JohnlyndelCastillo <johnlyndelcastillo@gmail.com>
2024-05-31 10:26:00 +0800

    applied initial dashboard html anddesign

--- view/dashboard.php
+++ view/dashboard.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-    </head>
-    <body>
-        <nav>
-            <a href="?logout=true">Logout</a>
-        </nav>
-        <div class="container">
-            <h1>This is user dashboard!</h1>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portal</title>
+    <link rel="stylesheet" href="css/dashboard_style.css">
+</head>
+
+<body>
+    <nav class="nav-bar">
+        <a href="?logout=true">Logout</a>
+        <h1>Pledge Portal System</h1>
+    </nav>
+    <div class="container">
+        <div class="side-nav">
+            <div class="side-nav-links">
+                <a href="#">My Profile</a>
+            </div>
+            <div class="side-nav-links">
+                <a href="#">Transaction History</a>
+            </div>
+            <div class="side-nav-links">
+                <a href="#">Pledge Payment</a>
+            </div>
+            <div class="side-nav-links">
+                <a href="#">Pending Payments</a>
+            </div>
         </div>
-    </body>
+
+        <div class="user-main-content">
+            <h1>Welcome to the Portal,[User] </h1>
+        </div>
+    </div>
+</body>
+
 </html>
