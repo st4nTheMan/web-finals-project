3f661461c05ab88b4e88c60d0990656c176f95b0
Kenji <146823959+Kenzoid12@users.noreply.github.com>
2024-05-31 23:16:16 +0800

    Add files via upload
    ako lng sa i save ari

---
+++ view/payment_method.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/payment_method_style.css">
+    <title>Payment Method</title>
+</head>
+<body>
+    <div id="payment">
+        <div class="container">
+            <div class="payment-form">
+                <form action="" method="post" id="payment-form">
+                    <h1>Payment Method</h1>
+                    <div class="parent_share">
+                        <h3>Parents Share</h3>
+                        <div class="form-group">
+                            <p>Enter Amount</p> 
+                        </div>
+                        <div class="form-group">
+                            <input type="number" name="amount" id="amount" require>
+                        </div>
+                        <div class="form-group">
+                            <button class="btn" type="submit" name="proceed"> Proceed</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
