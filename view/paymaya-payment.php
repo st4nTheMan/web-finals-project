3993311993eab808ea2881b9ff9f64868ad1ca69
JohnlyndelCastillo <johnlyndelcastillo@gmail.com>
2024-06-01 17:11:00 +0800

    Added paymaya payment option with css included

---
+++ view/paymaya-payment.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>PayMaya</title>
+    <link rel="stylesheet" href="css/paymaya_payment_style.css" />
+</head>
+
+<body>
+    <div id="paymaya">
+        <div class="container">
+            <div class="paymaya-form">
+                <div class="paymaya-logo">
+                    <h1>PayMaya</h1>
+                </div>
+                <form action="">
+                    <br>
+                    <label class="login-note">Login to your PayMaya Account</label>
+                    <br />
+                    <br />
+                    <label class="input-label">Email or Mobile No.</label>
+                    <br />
+                    <input type="text" class="paymaya-input-fields" placeholder="Enter Email or Mobile No." />
+                    <br />
+                    <label class="input-label">Enter amount</label>
+                    <br />
+                    <input type="text" class="paymaya-input-fields" placeholder="Enter Amount" />
+                    <br />
+                    <label class="input-label">Password:</label>
+                    <br />
+                    <input type="password" class="paymaya-input-fields" placeholder="Password" />
+                    <br />
+                    <button class="paymaya-login-btn">Login</button>
+                    <button class="back-btn" onclick="history.back()">
+                        < Go Back</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
